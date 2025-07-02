f68abd57d11f767d5c1a06e07a49cdefbb31b393
FooGiGo <felipelazcanocansino@gmail.com>
2025-07-02 16:24:35 -0400

    ps5 node v1

--- src/ps5_controller/src/ps5_node.py
+++ src/ps5_controller/src/ps5_node.py
@@ -1,37 +1,72 @@
 import rclpy
-import rclpy.node import Node
+from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Twist
 import pygame
 
-left_x = joystick.get_axis(0)
-left_y = -joystick.get_axis(1)
-right_x = joystick.get_axis(3)
-right_y = -joystick.get_axis(4)
-left_button = joystick.get_button(11)
-right_button = joystick.get_button(12)
+pygame.init()
+pygame.joystick.init()
 
-left_trigger = joystick.get_axis(2)
-right_trigger = joystick.get_axis(5)
-left_trigger_button = joystick.get_button(6)
-right_trigger_button = joystick.get_button(7)
-
-button_cross = joystick.get_button(0)
-button_circle = joystick.get_button(1)
-button_triangle = joystick.get_button(2)
-button_square = joystick.get_button(3)
-
-left_bumper = joystick.get_button(4)
-right_bumper = joystick.get_button(5)
-
-d_pad = joystick.get_hat(0)
-
-share_button = joystick.get_button(8)
-options_button = joystick.get_button(9)
-ps_button = joystick.get_button(10)
-
-class ps5_controller(Node):
+class Ps5Controller(Node):
     def __init__(self):
         super().__init__('ps5_node')
         self.rover_pub = self.create_publisher(Twist, '/cmd_wheel', 10)
         self.arm_pub = self.create_publisher(JointState, '/cmd_joint', 10)
+        self.ps5 = pygame.joystick.Joystick(0)
+        self.ps5.init()
+
+    def controller_callback(self):
+        button_layout = {
+            'left_x': ps5.get_axis(0),
+            'left_y': -ps5.get_axis(1),
+            'right_x': ps5.get_axis(3),
+            'right_y': -ps5.get_axis(4),
+            'left_button': ps5.get_button(11),
+            'right_button': ps5.get_button(12),
+
+            'left_trigger': ps5.get_axis(2),
+            'right_trigger': ps5.get_axis(5),
+            'left_trigger_button': ps5.get_button(6),
+            'right_trigger_button': ps5.get_button(7),
+
+            'button_cross': ps5.get_button(0),
+            'button_circle': ps5.get_button(1),
+            'button_triangle': ps5.get_button(2),
+            'button_square': ps5.get_button(3),
+
+            'left_bumper': ps5.get_button(4),
+            'right_bumper': ps5.get_button(5),
+
+            'd_pad': ps5.get_hat(0),
+
+            'share_button': ps5.get_button(8),
+            'options_button': ps5.get_button(9),
+            'ps_button': ps5.get_button(10)
+        }
+        twist = Twist()
+        twist.linear.x = float(button_layout['left_y'])
+        twist.angular.y = float(button_layout['right_x'])
+
+        joint_state = JointState()
+        joint_state.name = [f'joint{i+1}' for i in range(2)]
+        joint_state.velocity = [
+            float(button_layout['left_x']),
+            float(button_layout['right_y'])
+        ]
+        joint_state.position = []
+        joint_state.effort = []
+
+        self.arm_pub.publish(joint_state)
+        self.rover_pub.publish(twist)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('ps5_controller')
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
