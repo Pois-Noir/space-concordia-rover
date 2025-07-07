4b9f42f2b14d23e6a1505acfa35bc1e4d141bd85
FooGiGo <felipelazcanocansino@gmail.com>
2025-07-06 23:25:49 -0400

    Created a gps topic with desired lat long alt

--- src/gps/gps/added_gps_node.py
+++ src/gps/gps/added_gps_node.py
@@ -4,7 +4,7 @@ from sensor_msgs.msg import NavSatFix
 
 class GPS(Node):
     def __init__(self):
-        super.__init__('added_gps_node')
+        super().__init__('added_gps_node')
         self.gps_pub = self.create_publisher(NavSatFix, 'added_gps', 10)
         self.create_timer(0.1, self.publish_gps)
 
@@ -15,7 +15,7 @@ class GPS(Node):
         msg.altitude = 70.0
         msg.position_covariance = [0.0 for i in range(9)]
         msg.position_covariance_type = 3
-        self.publisher_.publish(msg)
+        self.gps_pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
---
+++ src/gps/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'gps'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Simple GPS publisher node using NavSatFix',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'added_gps_node = gps.added_gps_node:main',
+        ],
+    },
+)
